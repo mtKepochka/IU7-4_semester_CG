46b5144f60c6d7141cb493805e1bf8f6cde9a999
Kepochka <mt.kepochka@gmail.com>
2024-05-27 16:44:43 +0300

    ui done

--- lab_05/main.cpp
+++ lab_05/main.cpp
@@ -6,6 +6,11 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    w.setMaximumHeight(w.height());
+    w.setMaximumWidth(w.width());
+    w.setMinimumHeight(w.height());
+    w.setMinimumWidth(w.width());
+    w.setWindowTitle("Лабораторная работа #5");
     w.show();
     return a.exec();
 }
